1067c385552983347cf3cb9176b3f1f9a69a19ae
ChaosInTheCRD <tom@wksmac151488.home>
2022-07-07 16:32:00 +0100

    asdajshn

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	fmt.Println("check, check, 1-2-3, test 1-2-3, check 1-2-1-2...")
-        fmt.Println("Give it another check, 2-2-1-2, yeah yeah yeah")
+        fmt.Println("another check, 2-2-1-2, yeah yeah yeah")
 
 	file, err := os.Open("/rand.txt")
     if err != nil {
