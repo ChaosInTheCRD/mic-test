7a64d59f05df3e94d87d27d9f4c81a8717836d4d
chaosinthecrd <thomas.meadows@jetstack.io>
2022-07-21 09:56:10 +0100

    let's roll!

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 	fmt.Println("check, check, 1-2-3, test 1-2-3, check 1-2-1-2...")
         fmt.Println("another check, 2-2-1-2, oi oi yeah yeah")
 
+        fmt.Println("I am now officially attestagon'd")
+
 	file, err := os.Open("/rand.txt")
     if err != nil {
         log.Fatal(err)
