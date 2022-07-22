577288deb03abc02eeed1aa6d69395e40295fca9
chaosinthecrd <thomas.meadows@jetstack.io>
2022-07-22 15:54:40 +0100

    cool

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("check, check, 1-2-3, test 1-2-3, check 1-2-1-2..")
 
         for {
-        fmt.Println("I am now officially attestagon'd")
+        fmt.Println("I am now officially attestagon attested")
         time.Sleep(2 * time.Second)
      }
 }
