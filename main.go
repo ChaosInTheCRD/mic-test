bf930f0648d9041823c62e683ee99460840eae98
chaosinthecrd <thomas.meadows@jetstack.io>
2022-09-22 14:00:37 +0100

    adding change

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println("check, check, 1-2-3, test 1-2-3, check 1-2-1-2..")
+	fmt.Println("can you sign two - check, check, 1-2-3, test 1-2-3, check 1-2-1-2..")
         for {
         fmt.Println("I am now officially attestagon attested")
         time.Sleep(2 * time.Second)
