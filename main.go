00bba12ce24f1f3297393384b145106c1d3cc961
chaosinthecrd <tom@tmlabs.co.uk>
2023-04-26 15:10:08 +0100

    cool

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	fmt.Println("can you sign two - check, check, 1-2-3, test 1-2-3, check 1-2-1-2..")
-        for {
-        fmt.Println("I am now officially attestagon attested")
-        time.Sleep(2 * time.Second)
-     }
+	fmt.Println("check, check, 1-2-3, test 1-2-3, check 1-2-1-2..")
+	for {
+		fmt.Println("I am now officially attestagon attested")
+		time.Sleep(2 * time.Second)
+	}
 }
