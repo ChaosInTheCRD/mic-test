f367bf67769a5808508ce8322d481a12ad885b5b
chaosinthecrd <tom@tmlabs.co.uk>
2023-05-16 19:33:10 +0100

    let's go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	fmt.Println("check, check, 1-2-3, test 1-2-3, check 1-2-1-2..")
 	for {
-		fmt.Println("Testing Notation")
+		fmt.Println("Time to test all three!")
 		time.Sleep(2 * time.Second)
 	}
 }
