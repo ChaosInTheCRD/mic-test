8ed2d0c29321fe913e22a0c7ffdc77b430162668
chaosinthecrd <tom@tmlabs.co.uk>
2023-07-20 15:49:36 +0100

    test

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	fmt.Println("check, check, 1-2-3, test 1-2-3, check 1-2-1-2..")
 	for {
-		fmt.Println("going again.")
+		fmt.Println("one more time.")
 		time.Sleep(2 * time.Second)
 	}
 }
