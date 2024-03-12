47eedce5853dabab811917a50c9ad50ad1b07428
chaosinthecrd <tom@tmlabs.co.uk>
2024-03-12 11:55:39 +0000

    osdjh
    Signed-off-by: chaosinthecrd <tom@tmlabs.co.uk>

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	fmt.Println("check, check, 1-2-3, test 1-2-3, check 1-2-1-2..")
+	fmt.Println("blah blah blah")
 	for {
 		fmt.Println("one more time.")
 		time.Sleep(2 * time.Second)
